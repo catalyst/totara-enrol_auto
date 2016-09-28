cfaf64d81c81d0309b973afa65ce4b623a4c841f
Eugene Venter <eugene@catalyst.net.nz>
2016-09-29 10:04:28 +1300

    version.php: add maturity and release placeholders

--- version.php
+++ version.php
@@ -27,3 +27,6 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version   = 2016060900;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2015051100;        // Requires this Moodle version
 $plugin->component = 'enrol_auto';      // Full name of the plugin (used for diagnostics)
+$plugin->maturity  = MATURITY_ALPHA;
+$plugin->release = 'bleeding';
+
