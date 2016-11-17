a0650e1ea18adcffb4bc5115ef63df5df0987699
Eugene Venter <eugene@catalyst.net.nz>
2016-11-17 14:53:15 +1300

    lib.php: fix user enrolment edit link

--- lib.php
+++ lib.php
@@ -327,7 +327,7 @@ class enrol_auto_plugin extends enrol_plugin {
                 array('class' => 'unenrollink', 'rel' => $ue->id));
         }
         if ($this->allow_manage($instance) && has_capability("enrol/auto:manage", $context)) {
-            $url = new moodle_url('/enrol/auto/editenrolment.php', $params);
+            $url = new moodle_url('/enrol/editenrolment.php', $params);
             $actions[] = new user_enrolment_action(new pix_icon('t/edit', ''), get_string('edit'), $url,
                 array('class' => 'editenrollink', 'rel' => $ue->id));
         }
